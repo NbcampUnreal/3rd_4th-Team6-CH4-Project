1bfffcaa1c93c06451a58ed182f4182215ccea96
Parkjunbeom99 <pjb990@gmail.com>
2025-09-19 20:34:43 +0900

    [Update] 캐릭터 Death Ability 수정

--- CopAndRobber/Source/CopAndRobber/GAS/Ability/Character/GAP_Death.cpp
+++ CopAndRobber/Source/CopAndRobber/GAS/Ability/Character/GAP_Death.cpp
@@ -6,6 +6,7 @@
 #include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
 #include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
 #include "Character/CRCharacter.h"
+#include "GAS/CRAbilitySystemComponent.h"
 #include "GAS/GameplayTagsStatic.h"
 
 UGAP_Death::UGAP_Death()
@@ -24,17 +25,40 @@ void UGAP_Death::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const 
 	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
-	
+    
 	ACRCharacter* Character = Cast<ACRCharacter>(ActorInfo->AvatarActor.Get());
-	if (!Character) return;
+	if (!Character) 
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
+		return;
+	}
 
-
+	
 	Character->GetWorldTimerManager().SetTimer(DeathTimerHandle, [Character]()
-		{
-			Character->SetActorHiddenInGame(true);
-		}, DeathTime, false);
+	{
+		Character->SetActorHiddenInGame(true);
+	}, DeathTime, false);
 
 	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
 
+void UGAP_Death::EndDeathAbility()
+{
+	ACRCharacter* Character = GetOwner();
+	if (!Character)
+		return;
+	
+	Character->SetActorHiddenInGame(true);
+    
 
+	if (K2_HasAuthority())
+	{
+		if (UCRAbilitySystemComponent* ASC = Character->GetCRAbilitySystemComponent())
+		{
+			ASC->RemoveAllAbilities();
+		}
+	}
+
+	
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
