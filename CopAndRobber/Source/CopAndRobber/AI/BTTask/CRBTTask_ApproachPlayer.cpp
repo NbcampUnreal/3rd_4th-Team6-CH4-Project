c62eb0386616e62e8c6b27be9a3fa4c84755745b
jin945 <wlsrnjs945@naver.com>
2025-09-22 17:40:31 +0900

    Update AI끼리 행동 타이머 공유하던 문제 수정

--- CopAndRobber/Source/CopAndRobber/AI/BTTask/CRBTTask_ApproachPlayer.cpp
+++ CopAndRobber/Source/CopAndRobber/AI/BTTask/CRBTTask_ApproachPlayer.cpp
@@ -5,78 +5,94 @@
 
 UCRBTTask_ApproachPlayer::UCRBTTask_ApproachPlayer()
 {
-    bNotifyTick = true; 
+	bNotifyTick = true;
 }
 
 EBTNodeResult::Type UCRBTTask_ApproachPlayer::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-    ACRAIController* AICon = Cast<ACRAIController>(OwnerComp.GetAIOwner());
-    if (!AICon || !AICon->HasAuthority())
-    {
-        return EBTNodeResult::Failed;
-    }
+	FApproachPlayerTaskMemory* TaskMemory = (FApproachPlayerTaskMemory*)NodeMemory;
+	TaskMemory->bFinished = false;
 
-    UBlackboardComponent* BBComp = AICon->GetBlackboardComponent();
-    if (!BBComp)
-    {
-        return EBTNodeResult::Failed;
-    }
-    FVector PlayerLocation = BBComp->GetValueAsVector(ACRAIController::BBKey_PlayerLocation);
-    if (PlayerLocation.IsNearlyZero())
-    {
-        return EBTNodeResult::Failed;
-    }
-    BBComp->SetValueAsVector(ACRAIController::BBKey_TargetLocation, PlayerLocation);
-    AICon->MoveToLocation(PlayerLocation, -1.f, false, true, false, false, nullptr, true);
+	ACRAIController* AICon = Cast<ACRAIController>(OwnerComp.GetAIOwner());
+	if (!AICon || !AICon->HasAuthority())
+	{
+		return EBTNodeResult::Failed;
+	}
 
-   
-    if (OwnerComp.GetWorld())
-    {
-        OwnerComp.GetWorld()->GetTimerManager().SetTimer(TimerHandle, [this, &OwnerComp]()
-        {
-            OnApproachTimerFinished(&OwnerComp);
-        }, ApproachDuration, false);
-    }
+	UBlackboardComponent* BBComp = AICon->GetBlackboardComponent();
+	if (!BBComp)
+	{
+		return EBTNodeResult::Failed;
+	}
 
-    return EBTNodeResult::InProgress;
+	FVector PlayerLocation = BBComp->GetValueAsVector(ACRAIController::BBKey_PlayerLocation);
+	if (PlayerLocation.IsNearlyZero())
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	BBComp->SetValueAsVector(ACRAIController::BBKey_TargetLocation, PlayerLocation);
+	AICon->MoveToLocation(PlayerLocation, 150.f, false, true, false, false, nullptr, true);
+
+	if (UWorld* World = OwnerComp.GetWorld())
+	{
+		FTimerDelegate TimerDel;
+		TimerDel.BindUObject(this, &UCRBTTask_ApproachPlayer::OnApproachTimerFinished, &OwnerComp, TaskMemory);
+		World->GetTimerManager().SetTimer(TaskMemory->TimerHandle, TimerDel, ApproachDuration, false);
+	}
+
+	return EBTNodeResult::InProgress;
 }
 
 void UCRBTTask_ApproachPlayer::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
-    ACRAIController* AICon = Cast<ACRAIController>(OwnerComp.GetAIOwner());
-    if (!AICon)
-    {
-        return;
-    }
-    UBlackboardComponent* BBComp = AICon->GetBlackboardComponent();
-    if (!BBComp)
-    {
-        return;
-    }
-    FVector PlayerLocation = BBComp->GetValueAsVector(ACRAIController::BBKey_PlayerLocation);
-    if (PlayerLocation.IsNearlyZero())
-    {
-        return;
-    }
-    FVector CurrentTarget = BBComp->GetValueAsVector(ACRAIController::BBKey_TargetLocation);
-    if (!PlayerLocation.Equals(CurrentTarget, 10.f))
-    {
-        BBComp->SetValueAsVector(ACRAIController::BBKey_TargetLocation, PlayerLocation);
-        AICon->MoveToLocation(PlayerLocation, -1.f, false, true, false, false, nullptr, true);
-    }
+	FApproachPlayerTaskMemory* TaskMemory = (FApproachPlayerTaskMemory*)NodeMemory;
+	if (TaskMemory->bFinished)
+	{
+		return;
+	}
+
+	ACRAIController* AICon = Cast<ACRAIController>(OwnerComp.GetAIOwner());
+	if (!AICon)
+	{
+		return;
+	}
+
+	UBlackboardComponent* BBComp = AICon->GetBlackboardComponent();
+	if (!BBComp)
+	{
+		return;
+	}
+
+	FVector PlayerLocation = BBComp->GetValueAsVector(ACRAIController::BBKey_PlayerLocation);
+	if (PlayerLocation.IsNearlyZero())
+	{
+		return;
+	}
+
+	FVector CurrentTarget = BBComp->GetValueAsVector(ACRAIController::BBKey_TargetLocation);
+	if (!PlayerLocation.Equals(CurrentTarget, 10.f))
+	{
+		BBComp->SetValueAsVector(ACRAIController::BBKey_TargetLocation, PlayerLocation);
+		AICon->MoveToLocation(PlayerLocation, 150.f, false, true, false, false, nullptr, true);
+	}
 }
 
-void UCRBTTask_ApproachPlayer::OnApproachTimerFinished(UBehaviorTreeComponent* OwnerComp)
+void UCRBTTask_ApproachPlayer::OnApproachTimerFinished(UBehaviorTreeComponent* OwnerComp, FApproachPlayerTaskMemory* TaskMemory)
 {
-    if (!OwnerComp)
-    {
-        return;
-    }
-    ACRAIController* AICon = Cast<ACRAIController>(OwnerComp->GetAIOwner());
-    if (!AICon)
-    {
-        return;
-    }
-    AICon->ResetActionIndex(); 
-    FinishLatentTask(*OwnerComp, EBTNodeResult::Succeeded);
+	if (!OwnerComp)
+	{
+		return;
+	}
+
+	ACRAIController* AICon = Cast<ACRAIController>(OwnerComp->GetAIOwner());
+	if (!AICon)
+	{
+		return;
+	}
+
+	AICon->StopMovement();
+	AICon->ResetActionIndex();
+	TaskMemory->bFinished = true;
+	FinishLatentTask(*OwnerComp, EBTNodeResult::Succeeded);
 }
--- CopAndRobber/Source/CopAndRobber/AI/BTTask/CRBTTask_ApproachPlayer.h
+++ CopAndRobber/Source/CopAndRobber/AI/BTTask/CRBTTask_ApproachPlayer.h
@@ -4,6 +4,15 @@
 #include "BehaviorTree/BTTaskNode.h"
 #include "CRBTTask_ApproachPlayer.generated.h"
 
+USTRUCT()
+struct FApproachPlayerTaskMemory
+{
+	GENERATED_BODY()
+
+	bool bFinished = false;
+	FTimerHandle TimerHandle;
+};
+
 UCLASS()
 class COPANDROBBER_API UCRBTTask_ApproachPlayer : public UBTTaskNode
 {
@@ -13,15 +22,15 @@ public:
 	UCRBTTask_ApproachPlayer();
 
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
-
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual uint16 GetInstanceMemorySize() const override
+	{
+		return sizeof(FApproachPlayerTaskMemory);
+	}
 
 protected:
 	UPROPERTY(EditAnywhere, Category = "AI")
-	float ApproachDuration = 5.f;
+	float ApproachDuration = 3.f;
 
-	void OnApproachTimerFinished(UBehaviorTreeComponent* OwnerComp);
-
-private:
-	FTimerHandle TimerHandle;
+	void OnApproachTimerFinished(UBehaviorTreeComponent* OwnerComp, FApproachPlayerTaskMemory* TaskMemory);
 };
